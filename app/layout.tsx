bfb4621cc96d747f9410b1167cbf8c1ee3118775
Trevor Levinson <trevor.levinson@gmail.com>
2024-01-10 14:22:46 -0800

    Configure ESLint with Airbnb style guide and Prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,19 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Trevor Levinson Development',
-  description: 'A personal portfolio site for Trevor Levinson.',
-}
+  title: "Trevor Levinson Development",
+  description: "A personal portfolio site for Trevor Levinson.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className="overflow-hidden">{children}</body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
-import Base64TickerTape from "@components/Base64TickerTape"
-
-const Home = () => {
+function Home() {
   return (
     <main>
       <h1>Trevor Levinson Development</h1>
-      <div>
-        <Base64TickerTape />
-      </div>
     </main>
-  )
+  );
 }
 
-export default Home
+export default Home;
